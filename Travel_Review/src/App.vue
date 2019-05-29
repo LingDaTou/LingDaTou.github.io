ee819c0791852c79eeff594f6029308cb1fd1589
ling <153861690@qq.com>
2019-05-29 21:04:52 +0800

    add project

---
+++ Travel_Review/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+s
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
---
+++ Travel_Review/src/pages/Home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="home">
+		home
+	</div>	
+</template>
+
+<script type="text/javascript">
+	export default{
+		name:"Home"
+	}
+</script>
+
+<style scoped>
+.home{
+	font-size: 30px
+}
+</style>
