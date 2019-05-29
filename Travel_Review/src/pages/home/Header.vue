72b1975df8b2c1d1d4b4b3cea6e231c01f82cd5f
ling <153861690@qq.com>
2019-05-29 21:55:43 +0800

    update project

---
+++ Travel_Review/src/pages/home/Header.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="header">
+		hello world
+	</div>	
+</template>
+
+<script type="text/javascript">
+	export default{
+		name:"HomeHeader"
+	}
+</script>
+
+<style scoped>
+
+</style>
