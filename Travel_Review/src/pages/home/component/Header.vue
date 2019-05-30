80f77f33b418cea674d1854aa8618064517d5851
ling <153861690@qq.com>
2019-05-30 17:50:36 +0800

    add HomeHeader

--- Travel_Review/src/pages/home/component/Header.vue
+++ Travel_Review/src/pages/home/component/Header.vue
@@ -1,8 +1,14 @@
 <template>
 	<div class="header">
-		<div class="header-left">返回</div>
-        <div class="header-input">输入城市/景点/游玩主题</div>
-        <div class="header-right">城市</div>
+		<div class="header-left">
+            <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            输入城市/景点/游玩主题</div>
+        <div class="header-right">
+            城市<span class="iconfont arrow-icon">&#xe64a;</span>
+            </div>
 	</div>	
 </template>
 
@@ -13,6 +19,8 @@
 </script>
 
 <style lang="stylus" scoped>
+// @import '~@/assets/styles/varibles.styl'
+
 .header
     line-height 0.86rem
     display:flex
@@ -21,6 +29,9 @@
     .header-left
         width: 0.64rem
         float :left
+        .back-icon
+            text-align :center
+            font-size :.4rem
     .header-input
         flex:1
         background :#fff
@@ -28,10 +39,14 @@
         line-height :.64rem
         height :.64rem
         margin-left :.2rem
+        padding-left:.2rem
         border-radius :.1rem
         color:#ccc
     .header-right
         width:1.24rem
         float :right
         text-align :center
+        .arrow-icon
+            font-size :.24rem
+            padding-left:-.04rem
 </style>
