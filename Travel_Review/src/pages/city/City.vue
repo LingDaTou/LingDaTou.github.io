7935f4319883b22f135032b3ce6df76ca03b4e33
ling <153861690@qq.com>
2019-06-04 16:14:35 +0800

    add city-header

---
+++ Travel_Review/src/pages/city/City.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="city">
+        <city-header></city-header>
+    </div>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+export default {
+    name:"City",
+    components:{
+        CityHeader
+    }
+}
+</script>
+
+
+<style lang="stylus"  scoped>
+
+</style>
+
---
+++ Travel_Review/src/pages/city/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="header">
+          城市选择
+        <router-link to="/">
+            <div class="icon-back iconfont">
+                &#xe624;
+            </div>
+        </router-link>
+	</div>	
+</template>
+
+<script type="text/javascript">
+	export default{
+        name:"CityHeader",
+        props:{
+            city:String
+        }
+	}
+</script>
+
+<style lang="stylus" scoped>
+// @import '~@/assets/styles/varibles.styl'
+
+.header
+    position :relative
+    line-height 0.86rem
+    height :.86rem
+    background :#00bcd4
+    color:#fff
+    text-align :center
+    font-size :.32rem
+   .icon-back
+       position:absolute
+       top:0
+       left:0
+       width :.64rem
+       text-align :center
+       font-size :.4rem
+       color :#fff
+</style>
