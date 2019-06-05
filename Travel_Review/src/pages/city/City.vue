bb1188af9412ebbc4ae3913d971247336790bf03
ling <153861690@qq.com>
2019-06-05 15:58:46 +0800

    add city-components

--- Travel_Review/src/pages/city/City.vue
+++ Travel_Review/src/pages/city/City.vue
@@ -2,8 +2,8 @@
     <div class="city">
         <city-header></city-header>
         <city-search></city-search>
-        <city-list :cities="cities" :hotCities="hotCities"></city-list>
-        <city-alphabet :cities="cities"></city-alphabet>
+        <city-list :cities="cities" :hotCities="hotCities" :letter="letter"></city-list>
+        <city-alphabet :cities="cities" @change="handleChange"></city-alphabet>
     </div>
 </template>
 
@@ -18,7 +18,8 @@ export default {
     data(){
         return{
             cities:{},
-            hotCities:[]
+            hotCities:[],
+            letter:''
         }
     },
     components:{
@@ -39,6 +40,10 @@ export default {
                 this.cities=data.cities
                 this.hotCities=data.hotCities
             }
+        },
+        handleChange(letter){
+            // console.log(letter)
+            this.letter=letter
         }
     },
     mounted(){
--- Travel_Review/src/pages/city/components/List.vue
+++ Travel_Review/src/pages/city/components/List.vue
@@ -19,7 +19,9 @@
                 </div>
             </div>
 
-        <div class="area" v-for="(item,key) of cities" :key="key">
+        <div class="area" v-for="(item,key) of cities" :key="key"
+            :ref="key"
+        >
             <div class="title border-topbottom">{{key}}</div>
             <div class="item-list">
                 <div class="item border-bottom"
@@ -38,10 +40,20 @@ import BScroll from "better-scroll"
     name:"CitySearch",
     props:{
         cities:Object,
-        hotCities:Array
+        hotCities:Array,
+        letter:String
     },
     mounted() {
         this.scroll = new BScroll(this.$refs.wrapper)
+    },
+    watch:{
+        letter(){
+            if(this.letter){
+                const element=this.$refs[this.letter][0]
+                // console.log(element)
+                this.scroll.scrollToElement(element)
+            }
+        }
     }
 }
 </script>
